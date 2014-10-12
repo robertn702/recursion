1a24b54ef421e6db7c26f04b0ff5c36f8b002601
Robert <robertn702@gmail.com>
2014-10-12 20:43:51 +1100

    fixed getElementsByClassName()

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
-};
+var getElementsByClassName = function (className) {
+	var outputArray = [];
+	
+	var findElement = function (searchNode) {
+		for (var i = 0 ; i < searchNode.length ; i++) {
+			if (typeof searchNode[i].className === "string" && searchNode[i].className.indexOf(className) + 1) {
+				outputArray.push (searchNode[i]);
+			}
+
+			if (searchNode[i].childNodes.length > 0) {
+				findElement (searchNode[i].childNodes);
+			}
+		}
+		return outputArray;
+	}
+
+	if (document.body.className === className) {
+		outputArray.push (document.body);
+	} 
+
+	if (document.body.classList.contains(className)) {
+		return findElement (document.body.childNodes);
+	} else {
+		return outputArray;
+	}
+}
